112f6f044dbed77146b1f558214b87e9aa82d18d
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-06-10 16:21:49 +0530

    Create rearrange_array.cpp

---
+++ Math/rearrange_array.cpp
@@ -0,0 +1,17 @@
+// https://www.interviewbit.com/problems/rearrange-array/
+
+// using extra memory - not accepted in real programming
+void Solution::arrange(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int n = A.size();
+    vector<int> ans(n);
+    
+    for(int i=0;i<n;++i)
+        ans[i] = A[A[i]];
+    
+    for(int i=0;i<n;++i)
+        A[i] = ans[i];
+}
