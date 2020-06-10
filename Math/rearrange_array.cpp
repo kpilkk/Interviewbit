3a6739fe9768b6c911be075bb4c4ff3cfd450b7e
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-06-10 17:08:08 +0530

    Update rearrange_array.cpp

--- Math/rearrange_array.cpp
+++ Math/rearrange_array.cpp
@@ -1,5 +1,23 @@
 // https://www.interviewbit.com/problems/rearrange-array/
 
+// Acceptable solution
+// idea is to store two numbers in one number
+void Solution::arrange(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int n = A.size();
+    
+    for(int i=0;i<n;++i){
+        A[i] = (A[i]%n)+(A[A[i]]%n)*n;
+    }
+    
+    for(int i=0;i<n;++i){
+        A[i] /= n;
+    }
+}
+
 // using extra memory - not accepted in real programming
 void Solution::arrange(vector<int> &A) {
     // Do not write main() function.
