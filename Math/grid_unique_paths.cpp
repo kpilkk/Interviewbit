a9e996f16e7b4068b797aaca8cd914aba4146f54
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-06-10 18:20:15 +0530

    Using dp updated

--- Math/grid_unique_paths.cpp
+++ Math/grid_unique_paths.cpp
@@ -26,12 +26,11 @@ int Solution::uniquePaths(int A, int B) {
 
 // Using DP
 int Solution::uniquePaths(int A, int B) {
-    vector<int> dp(A);
-    dp[0] = 1;
-    for(int i = 0; i < B; i++) {
-        for (int j = 1; j < A; j++) {
-            dp[j] += dp[j-1];
+    vector<vector<int>> dp(A, vector<int>(B, 1));
+    for (int i = 1; i < A; i++) {
+        for (int j = 1; j < B; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
     }
-    return dp[A-1];
+    return dp[A - 1][B - 1];
 }
