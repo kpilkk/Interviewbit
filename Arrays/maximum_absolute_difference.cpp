ff29acf88b3ab5c0fc4d3244ae049582a2995317
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-06-13 14:00:53 +0530

    Update maximum_absolute_difference.cpp

--- Arrays/maximum_absolute_difference.cpp
+++ Arrays/maximum_absolute_difference.cpp
@@ -1,5 +1,19 @@
 // https://www.interviewbit.com/problems/maximum-absolute-difference/
 
+// Working solution - O(n)
+int Solution::maxArr(vector<int> &A) {
+    int n = A.size();
+    
+    int max1 = INT_MIN, max2 = INT_MIN, min1 = INT_MAX, min2 = INT_MAX;
+    for(int i=0;i<n;++i){
+        max1 = max(max1, A[i]+i);
+        min1 = min(min1, A[i]+i);
+        max2 = max(max2, A[i]-i);
+        min2 = min(min2, A[i]-i);
+    }
+    return max(max1-min1, max2-min2);
+}
+
 // Time Limit Exceeded - brute force solution
 int Solution::maxArr(vector<int> &A) {
     int n = A.size();
