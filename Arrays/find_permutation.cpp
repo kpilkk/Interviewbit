59fbc7ca7d0b2aaca6384365ba1e99dc7e259638
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-06-14 12:18:40 +0530

    my solution using stack

--- Arrays/find_permutation.cpp
+++ Arrays/find_permutation.cpp
@@ -1,50 +1,33 @@
 // problem link: https://www.interviewbit.com/problems/find-permutation/
-/*
-Given a positive integer n and a string s consisting only of letters D or I, you have to find any permutation of first n positive integer that satisfy the given input string.
 
-D means the next number is smaller, while I means the next number is greater.
-
-Notes
-
-	1.Length of given string s will always equal to n - 1
-	2.Your solution should run in linear time and space.
-Example :
-
-Input 1:
-
-n = 3
-
-s = ID
-
-Return: [1, 3, 2]
-*/
-
-// Solution using stack
+// Using stack
 vector<int> Solution::findPerm(const string A, int B) {
     int n = A.size();
-    vector<int> ans(n+1);
+    if(n==0) return {1};
+
+    vector<int> ans;
     stack<int> s;
-    int j = 0;
-    for(int i=1;i<=n;++i){
-        if(A[i-1]=='I'){
-            s.push(i);
+    
+    int j = 1;
+    for(int i=0;i<n;++i){
+        if(A[i]=='I'){
+            s.push(j++);
             while(!s.empty()){
-                ans[j++] = s.top();
+                ans.push_back(s.top());
                 s.pop();
             }
         }
         else{
-            s.push(i);
+            s.push(j++);
         }
     }
-    s.push(n+1);
+    s.push(j);
     while(!s.empty()){
-        ans[j++] = s.top();
+        ans.push_back(s.top());
         s.pop();
     }
     return ans;
 }
-
 
 // Solution by reversing the subarray
 vector<int> Solution::findPerm(const string A, int B) {
