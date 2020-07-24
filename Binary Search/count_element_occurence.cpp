3aa1f24f5586fbe4a8f8bcf31279bba33a2b242d
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-07-24 11:57:38 +0530

    Update count_element_occurence.cpp

--- Binary Search/count_element_occurence.cpp
+++ Binary Search/count_element_occurence.cpp
@@ -2,32 +2,37 @@
 
 // Using binary search
 int Solution::findCount(const vector<int> &A, int B) {
-    int count = 0;
     int n = A.size();
-    int l=0, r=n-1;
-    int x = -1;
 
-    while(l<=r){
-        int mid = l+(r-l)/2;
+    int i = 0;
+    int j = n-1;
+    
+    int count = 0;
+    int mid_ = -1;
+    while(i<=j){
+        // i<=j is placed as it will fail for the case when n=1 and the B=A[0]
+        // A : [ 1 ]  B : 1
+        int mid = i+(j-i)/2;
         if(A[mid]==B){
-            x = mid;
+            mid_ = mid;
             break;
         }
-        if(A[mid]<B) l=mid+1;
-        else r=mid-1;
+        else if(A[mid]<B)
+            i = mid+1;
+        else
+            j = mid-1;
     }
     
-    if(x!=-1){
-        int i = x;
-        while(A[i]==B && i>=0){
-            ++count;
-            --i;
-        }
-        i = x+1;
-        while(A[i]==B && i<n){
-            ++count;
-            ++i;
-        }
+    if(mid_==-1) return 0;
+    
+    for(int i=mid_;i<n;++i){
+        if(A[i]==B) ++count;
+        else break;
+    }
+    for(int i=mid_-1;i>=0;--i){
+        if(A[i]==B) ++count;
+        else break;
     }
     return count;
 }
+
