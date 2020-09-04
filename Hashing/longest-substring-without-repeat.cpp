29de1904d2a209212144211a7387ae1e017e0964
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-04 19:43:12 +0530

    Update longest-substring-without-repeat.cpp

--- Hashing/longest-substring-without-repeat.cpp
+++ Hashing/longest-substring-without-repeat.cpp
@@ -1,5 +1,6 @@
 // https://www.interviewbit.com/problems/longest-substring-without-repeat/
 
+// 1st solution
 int Solution::lengthOfLongestSubstring(string A) {
     int n = A.length();
     int i = 0, j = 0, ans = 0;
@@ -13,6 +14,23 @@ int Solution::lengthOfLongestSubstring(string A) {
         else{
             temp.erase(A[i++]);
         }
+    }
+    return ans;
+}
+
+
+// Most optimized solution
+
+int Solution::lengthOfLongestSubstring(string A) {
+    int n = A.length(), ans = 0;
+    map<char, int> temp;
+    
+    for(int i = 0, j = 0; j < n; ++j) {
+        if(temp.find(A[j]) != temp.end()) {
+            i = max(i, temp[A[j]]);
+        }
+        ans = max(ans, j - i + 1);
+        temp[A[j]] = j + 1;
     }
     return ans;
 }
