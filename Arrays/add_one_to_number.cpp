a435fa281278cd39179888f03a5225f7513052f0
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-06 16:58:11 +0530

    Update add_one_to_number.cpp
    solution without reversing the vector

--- Arrays/add_one_to_number.cpp
+++ Arrays/add_one_to_number.cpp
@@ -29,3 +29,31 @@ vector<int> Solution::plusOne(vector<int> &A) {
     reverse(A.begin(), A.end());
     return A;
 }
+
+
+// Solution without reversing
+
+vector<int> Solution::plusOne(vector<int> &A) {
+    int n = A.size();
+    
+    int sum = A[n - 1] + 1;
+    A[n - 1] = sum % 10;
+    int carry = sum / 10;
+    int i = n - 2;
+    
+    while(carry && i >= 0){
+        sum = A[i] + carry;
+        A[i--] = sum % 10;
+        carry = sum / 10;
+    }
+    
+    if(carry)
+        A.emplace(A.begin(), carry);
+        
+    i = 0;
+    
+    while(A[i] == 0)
+        A.erase(A.begin());
+        
+    return A;
+}
