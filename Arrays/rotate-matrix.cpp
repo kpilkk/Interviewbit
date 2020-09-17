c9f1a7eedeecf3b1bade5f14d36814e660b90304
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-17 22:37:45 +0530

    Update rotate-matrix.cpp

--- Arrays/rotate-matrix.cpp
+++ Arrays/rotate-matrix.cpp
@@ -7,3 +7,17 @@ void Solution::rotate(vector<vector<int> > &A) {
         for(int j = i + 1; j < n; ++j)
             swap(A[i][j], A[j][i]);
 }
+
+// Interviewbit solution
+void Solution::rotate(vector<vector<int> > &A) {
+    int len = matrix.size();
+    for (int i = 0; i < len / 2; i++) {
+        for (int j = i; j < len - i - 1; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[len - j - 1][i];
+        matrix[len - j - 1][i] = matrix[len - i - 1][len - j - 1];
+        matrix[len - i - 1][len - j - 1] = matrix[j][len - i - 1];
+        matrix[j][len - i - 1] = tmp;
+        }
+    }
+}
