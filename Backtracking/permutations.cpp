2b9819512cea9f1f59ef81134f103792601eaf5a
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-22 15:15:22 +0530

    Update permutations.cpp
    solution using next permutation added

--- Backtracking/permutations.cpp
+++ Backtracking/permutations.cpp
@@ -1,5 +1,6 @@
 // https://www.interviewbit.com/problems/permutations/
 
+// recursive solution
 void permuteUnique(vector<int> A, int i,vector<vector<int>>& ans){
     if(i==A.size())
         ans.push_back(A);
@@ -16,5 +17,31 @@ void permuteUnique(vector<int> A, int i,vector<vector<int>>& ans){
 vector<vector<int> > Solution::permute(vector<int> &A) {
     vector<vector<int>> ans;
     permuteUnique(A, 0, ans);
+    return ans;
+}
+
+// non-recursive solution
+
+bool nextPermutation(vector<int>& A){
+    int i = A.size() - 2;
+    while(i >= 0 && A[i] >= A[i + 1])
+        --i;
+    if(i >= 0){
+        int j = A.size() - 1;
+        while(j >= 0 && A[j] <= A[i])
+            --j;
+        swap(A[i], A[j]);
+    }
+    else return false;
+    reverse(A.begin() + i + 1, A.end());
+    return true;
+}
+vector<vector<int> > Solution::permute(vector<int> &A) {
+    vector<vector<int>> ans;
+    sort(A.begin(), A.end());
+    do{
+        ans.push_back(A);
+    } while(nextPermutation(A));
+    
     return ans;
 }
