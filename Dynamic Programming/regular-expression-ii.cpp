26e3e801b39fc824656fee63440b47f3e74edefa
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-07 22:29:23 +0530

    Update regular-expression-ii.cpp
    DP solution added

--- Dynamic Programming/regular-expression-ii.cpp
+++ Dynamic Programming/regular-expression-ii.cpp
@@ -4,7 +4,7 @@
 int Solution::isMatch(const string A, const string B) {
     if(B.empty()) return A.empty();
     
-    int match = (A[0] == B[0]) || (B[0] == '.');
+    int match = !A.empty() && (A[0] == B[0]) || (B[0] == '.');
     
     if(B.size() >= 2 && B[1] == '*')
         return isMatch(A, B.substr(2)) || (match && isMatch(A.substr(1), B));
@@ -13,3 +13,18 @@ int Solution::isMatch(const string A, const string B) {
 }
 
 // DP solution
+int Solution::isMatch(const string A, const string B) {
+    int m = A.size(), n = B.size();
+    vector<vector<bool>> dp(m + 1, vector<bool> (n + 1, false));
+    dp[0][0] = true;
+    
+    for(int i = 0; i <= m; ++i){
+        for(int j = 1; j <= n; ++j){
+            if(B[j - 1] == '*')
+                dp[i][j] = dp[i][j - 2] || (i && dp[i - 1][j] && (A[i - 1] == B[j - 2] || B[j - 2] == '.'));
+            else
+                dp[i][j] = i && (A[i - 1] == B[j - 1] || B[j - 1] == '.') && dp[i - 1][j - 1];
+        }
+    }
+    return dp[m][n];
+}
