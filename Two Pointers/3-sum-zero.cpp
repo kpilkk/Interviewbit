fd0b639139356797ea4ef91f5e919f4390685703
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-13 13:54:00 +0530

    Create 3-sum-zero.cpp

---
+++ Two Pointers/3-sum-zero.cpp
@@ -0,0 +1,30 @@
+// https://www.interviewbit.com/problems/3-sum-zero/
+
+vector<vector<int> > Solution::threeSum(vector<int> &A) {
+    sort(A.begin(), A.end());
+    int n = A.size();
+    vector<vector<int>> ans;
+    for(int i = 0; i < n - 2; ++i){
+        if(i > 0 && A[i] == A[i - 1]) // for the case of duplicates
+            continue;
+        int l = i + 1, r = n - 1;
+
+        while(l < r){
+            // long long sum = A[i] + (long long)A[l] + (long long)A[r];
+            long long sum = 0LL + A[i] + A[l] + A[r];  // if sum overflows
+            if(sum == 0){
+                ans.push_back({A[i], A[l], A[r]});
+                while(l < n - 1 && A[l] == A[l + 1])  // for the case of duplicates
+                    ++l;
+                while(r > 0 && A[r] == A[r - 1])      // for the case of duplicates
+                    --r;
+                ++l, --r;
+            }
+            else if(sum > 0)
+                --r;
+            else
+                ++l;
+        }
+    }
+    return ans;
+}
