affeb0005e28a769331b31788ab4fa116dcfda4b
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-27 15:16:57 +0530

    Update implement-strstr.cpp

--- Strings/implement-strstr.cpp
+++ Strings/implement-strstr.cpp
@@ -22,3 +22,37 @@ int Solution::strStr(const string A, const string B) {
     }
     return -1;
 }
+
+// KMP algorithm
+vector<int> kmp(string needle){
+    int n = needle.length();
+    vector<int> lps(n , 0);
+    
+    for(int i = 1, len = 0; i < n;){
+        if(needle[i] == needle[len])
+            lps[i++] = ++len;
+        else if(len)
+            len = lps[len - 1];
+        else
+            lps[i++] = 0;
+    }
+    return lps;
+}
+
+int Solution::strStr(const string A, const string B) {
+    int m = A.length(), n = B.length();
+    
+    if(!n)
+        return -1;
+
+    vector<int> lps = kmp(B);
+    for(int i = 0, j = 0; i < m;){
+        if(A[i] == B[j])
+            ++i, ++j;
+        if(j == n)
+            return i - j;
+        if(i < m && A[i] != B[j])
+            j ? j = lps[j - 1] : i++;
+    }
+    return -1;
+}
