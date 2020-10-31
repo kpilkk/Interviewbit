3d069e235de636f0154e10d1fc767b6baf16e1fc
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-31 18:49:30 +0530

    Update longest-valid-parentheses.cpp

--- Dynamic Programming/longest-valid-parentheses.cpp
+++ Dynamic Programming/longest-valid-parentheses.cpp
@@ -38,3 +38,34 @@ int Solution::longestValidParentheses(string A) {
     }
     return ans;
 }
+
+// Without using extra space
+int Solution::longestValidParentheses(string A) {
+    int left = 0, right = 0, ans = 0, n = A.size();
+    
+    for(int i = 0; i < n; ++i){
+        if(A[i] == '(')
+            ++left;
+        else
+            ++right;
+        if(left == right)
+            ans = max(ans, 2 * right);
+        else if(right > left)
+            left = right = 0;
+    }
+    
+    left = right = 0;
+    
+    for(int i = n - 1; i >= 0; --i){
+        if(A[i] == '(')
+            ++left;
+        else
+            ++right;
+        if(left == right)
+            ans = max(ans, 2 * right);
+        else if(left > right)
+            left = right = 0;
+    }
+    
+    return ans;
+}
