bd3ce9913f2f832c200c2c994d73de10eb520feb
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-03 19:41:12 +0530

    Update search-for-a-range.cpp

--- Binary Search/search-for-a-range.cpp
+++ Binary Search/search-for-a-range.cpp
@@ -30,3 +30,24 @@ vector<int> Solution::searchRange(const vector<int> &A, int B) {
     
     return ans;
 }
+
+// Divide and conquer solution
+vector<int> search(const vector<int> &nums, int l, int r, int target){
+    if(nums[l] == target && nums[r] == target)
+        return {l, r};
+    if(nums[l] <= target && target <= nums[r]){
+        int mid = l + ((r - l) >> 1);
+        vector<int> left = search(nums , l, mid, target);
+        vector<int> right = search(nums , mid + 1, r, target);
+        if(left[0] == -1)
+            return right;
+        if(right[0] == -1)
+            return left;
+        return {left[0], right[1]};
+    }
+    return {-1 , -1};
+}
+vector<int> Solution::searchRange(const vector<int> &A, int B) {
+    int n = A.size();
+    return search(A, 0, n - 1, B);
+}
