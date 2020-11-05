8f0939c04878efcbdf7ca3a8338cd564995dfe65
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-05 19:43:29 +0530

    Update min-depth-of-binary-tree.cpp

--- Tree Data Structure/min-depth-of-binary-tree.cpp
+++ Tree Data Structure/min-depth-of-binary-tree.cpp
@@ -12,3 +12,29 @@ int Solution::minDepth(TreeNode* A) {
         
     return 1 + min(minDepth(A -> left), minDepth(A -> right));
 }
+
+// BFS iterative solution
+int Solution::minDepth(TreeNode* A) {
+    if(!A)
+        return 0;
+        
+    queue<TreeNode*> bfs;
+    bfs.push(A);
+    int ans = 0;
+    
+    while(!bfs.empty()){
+        int n = bfs.size();
+        ++ans;
+        for(int i = 0; i < n; ++i){
+            TreeNode* temp = bfs.front();
+            bfs.pop();
+            if(temp -> left == temp -> right)
+                return ans;
+            if(temp -> left)
+                bfs.push(temp -> left);
+            if(temp -> right)
+                bfs.push(temp -> right);
+        }
+    }
+    return ans;
+}
