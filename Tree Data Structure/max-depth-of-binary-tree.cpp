7c8cd6bfbfa6d577d76eff570d0862e924c1735c
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-05 15:18:26 +0530

    Create max-depth-of-binary-tree.cpp

---
+++ Tree Data Structure/max-depth-of-binary-tree.cpp
@@ -0,0 +1,18 @@
+// https://www.interviewbit.com/problems/max-depth-of-binary-tree/
+
+// DFS recursive
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::maxDepth(TreeNode* A) {
+    if(!A)
+        return 0;
+        
+    return 1 + max(maxDepth(A -> left), maxDepth(A -> right));
+}
