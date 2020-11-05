690f642d605a7d55a00b5efd15998f2543d396fc
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-05 16:20:30 +0530

    Update max-depth-of-binary-tree.cpp

--- Tree Data Structure/max-depth-of-binary-tree.cpp
+++ Tree Data Structure/max-depth-of-binary-tree.cpp
@@ -16,3 +16,27 @@ int Solution::maxDepth(TreeNode* A) {
         
     return 1 + max(maxDepth(A -> left), maxDepth(A -> right));
 }
+
+// BFS iterative
+int Solution::maxDepth(TreeNode* A) {
+    if(!A)
+        return 0;
+        
+    queue<TreeNode*> bfs;
+    bfs.push(A);
+    int ans = 0;
+    
+    while(!bfs.empty()){
+        ++ans;
+        int n = bfs.size();
+        for(int i = 0; i < n; ++i){
+            TreeNode* temp = bfs.front();
+            bfs.pop();
+            if(temp -> left)
+                bfs.push(temp -> left);
+            if(temp -> right)
+                bfs.push(temp -> right);
+        }
+    }
+    return ans;
+}
