dd1eab933824f24136a1aba3aee72e08cb72f797
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-06 13:32:46 +0530

    Update symmetric-binary-tree.cpp

--- Tree Data Structure/symmetric-binary-tree.cpp
+++ Tree Data Structure/symmetric-binary-tree.cpp
@@ -12,3 +12,29 @@ int isMirror(TreeNode* left, TreeNode* right){
 int Solution::isSymmetric(TreeNode* A) {
     return isMirror(A , A);
 }
+
+// BFS iterative solution
+int Solution::isSymmetric(TreeNode* A) {
+    if(!A)
+        return 1;
+    queue<TreeNode*> bfs;
+    bfs.push(A);
+    bfs.push(A);
+    while(!bfs.empty()){
+        auto left = bfs.front();
+        bfs.pop();
+        auto right = bfs.front();
+        bfs.pop();
+        if(!left && !right)
+            continue;
+        if(!left|| !right)
+            return 0;
+        if(left -> val != right -> val)
+            return false;
+        bfs.push(left -> left);
+        bfs.push(right -> right);
+        bfs.push(left -> right);
+        bfs.push(right  -> left);
+    }
+    return 1;
+}
