194aa958bc4fefa5d1247a82b008da25280c0003
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-06 14:31:38 +0530

    Update path-sum.cpp

--- Tree Data Structure/path-sum.cpp
+++ Tree Data Structure/path-sum.cpp
@@ -10,3 +10,24 @@ int Solution::hasPathSum(TreeNode* A, int B) {
     
     return hasPathSum(A -> left, B - A -> val) || hasPathSum(A -> right, B - A -> val);
 }
+
+// BFS iterative solution
+int Solution::hasPathSum(TreeNode* A, int B) {
+    if(!A)
+        return false;
+    
+    queue<pair<TreeNode*, int>> bfs;
+    bfs.push({A, 0});
+    while(!bfs.empty()){
+        auto temp = bfs.front();
+        bfs.pop();
+        int s = temp.first -> val + temp.second;
+        if(temp.first -> left == temp.first -> right && s == B)
+            return 1;
+        if(temp.first -> left)
+            bfs.push({temp.first -> left, s});
+        if(temp.first -> right)
+            bfs.push({temp.first -> right, s});
+    }
+    return 0;
+}
