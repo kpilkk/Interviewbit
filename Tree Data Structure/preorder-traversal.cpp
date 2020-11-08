b0469aab8d738f783b0b3b67f1d6db40e87f889c
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-08 10:14:07 +0530

    Update preorder-traversal.cpp

--- Tree Data Structure/preorder-traversal.cpp
+++ Tree Data Structure/preorder-traversal.cpp
@@ -14,3 +14,24 @@ vector<int> Solution::preorderTraversal(TreeNode* A) {
     helper(A, ans);
     return ans;
 }
+
+// Iterative solution using stack
+vector<int> Solution::preorderTraversal(TreeNode* A) {
+    vector<int> ans;
+    stack<TreeNode*> stack;
+    
+    while(A || !stack.empty()){
+        if(A){
+            ans.emplace_back(A -> val);
+            if(A -> right)
+                stack.emplace(A -> right);
+            A = A -> left;
+        }
+        else{
+            A = stack.top();
+            stack.pop();
+        }
+    }
+    
+    return ans;
+}
